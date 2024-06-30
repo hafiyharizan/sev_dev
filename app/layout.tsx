17459d1616a96b75bad10fa96e66dc9d39baef8d
Hafiy Harizan <99054638+hafiyharizan@users.noreply.github.com>
2024-07-01 04:35:44 +0800

    web analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/index';
-
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: 'SEV',
